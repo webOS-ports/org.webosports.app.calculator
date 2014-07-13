9a754f4a240f2ab159e4e6fa8c5a6f8ccf7972f4
john.microtech@gmail.com <john.microtech@gmail.com>
2014-07-13 15:46:39 -0400

    littel more code lay out fix
    Enyo-DCO-1.1 Signed-off-by: johnmcconnell@yahoo.com

--- source/App.js
+++ source/App.js
@@ -12,58 +12,57 @@ enyo.kind({
 			{kind: "Control", style: "-webkit-flex: 1; max-width: 550px;   display: -webkit-flex; -webkit-flex-direction: column; ", components:[
 			{name: "bezel", kind: "FittableRows", classes: "bezel",	components:[
 				{kind: "onyx.Toolbar", style: "margin-bottom: 5px;", components:[
-				{name: "Result", style: "font-size: 16pt; font-weight: bold;"}
-			]},
+					{name: "Result", style: "font-size: 16pt; font-weight: bold;"}
+				]},
 				{kind: "FittableColumns", style: "margin-bottom: 5px;", components:[
 					{kind: "onyx.Toolbar", style: "width: 75%; height: 48px; margin-left: 12.5%; margin-right: 5px; text-align: right;", fit: true,	components:[
 						{name: "Formula", style: "font-size: 12pt;"}
 					]},
 					{kind: "onyx.Button", style: "width: 48px; height: 48px; border-radius: 24px;", classes: "onyx-toolbar", content: "<", ontap: "backspaceTapped"}
 				]},
-				{kind: "FittableRows", fit: true, defaultKind: enyo.kind({kind: "FittableColumns", style: "height: 15.5%; margin: 0.5%;", defaultKind: enyo.kind({
-					kind: "onyx.Button", classes: "onyx-toolbar", style: "width: 24%; margin: 0.5%; border-radius: 8px; font-size: 24pt; font-weight: bold;", ontap: "keyTapped"
-				})
-				}),
-				components:[
-					{components:[
+				{kind: "FittableRows", fit: true,
+					defaultKind: enyo.kind({kind: "FittableColumns", style: "height: 15.5%; margin: 0.5%;",
+						defaultKind: enyo.kind({kind: "onyx.Button", classes: "onyx-toolbar", style: "width: 24%; margin: 0.5%; border-radius: 8px; font-size: 24pt; font-weight: bold;", ontap: "keyTapped"})
+					}),
+					components:[
+						{components:[
 					/**{style: "visibility: hidden;"},*/
 					{content: "\u221a", ontap: "sqrtTapped"},		
 					{content: "("},
 					{content: ")"},
 					{content: "C", style: "margin-right: 0;", ontap: "cancelTapped"}
 				]},
-					{components:[
+						{components:[
 					{content: "ln", ontap: "lnTapped"},
 					{content: "log", ontap: "logTapped"},
 					{content: "^2"},
 					{content: ""}
 				]},
-
-					{components:[
+						{components:[
 					{content: "7", classes: "number-button"},
 					{content: "8", classes: "number-button"},
 					{content: "9", classes: "number-button"},
 					{content: "+", style: "margin-right: 0;"}
 				]},
-					{components:[
+						{components:[
 					{content: "4", classes: "number-button"},
 					{content: "5", classes: "number-button"},
 					{content: "6", classes: "number-button"},
 					{content: "-", style: "margin-right: 0;"}
 				]},
-					{components:[
+						{components:[
 					{content: "1", classes: "number-button"},
 					{content: "2", classes: "number-button"},
 					{content: "3", classes: "number-button"},
 					{content: "*", style: "margin-right: 0;"}
 				]},
-					{components:[
+						{components:[
 					{content: "."},
 					{content: "0", classes: "number-button"},
 					{content: "=", ontap: "equalsTapped"},
 					{content: "/", style: "margin-right: 0;"}
 				]}
-				]}
+					]}
 				]}   // end bezel
 			]}
 		]},   // end tabletop
