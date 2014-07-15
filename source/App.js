28bddb40cd11c864126389afd911c4536ef3a6e4
john.microtech@gmail.com <john.microtech@gmail.com>
2014-07-15 14:42:06 -0400

    Formating fix  space/tab mix
    fixed my bug in %  *6 dose not become *6 but .06 now
    
    Enyo-DCO-1.1 Signed-off-by: johnmcconnell@yahoo.com

--- source/App.js
+++ source/App.js
@@ -9,76 +9,76 @@ enyo.kind({
 		onCoreNaviDrag: "handleCoreNaviDrag",
 		onCoreNaviDragFinish: "handleCoreNaviDragFinish"},
 		{name: "tabletop", kind: "Control", style:"-webkit-flex: 1;   display: -webkit-flex; -webkit-flex-direction: row; -webkit-justify-content: center", components: [
-		{kind: "Control", style: "-webkit-flex: 1; max-width: 550px;   display: -webkit-flex; -webkit-flex-direction: column;  -webkit-justify-content: center", components:[
-		{name: "bezel", kind: "FittableRows",
-		style: "-webkit-flex: 1; max-height: 665px; border: 5px solid #333; background-color: #777; padding: 10px; color: white; margin: 10px; border-radius: 16px; text-align: right;",
-		components:[
-			{kind: "onyx.Toolbar", style: "margin-bottom: 5px;", components:[
-				{name: "Result", style: "font-size: 16pt; font-weight: bold;"}
-			]},
-			{kind: "FittableColumns", style: "margin-bottom: 5px;", components:[
-				{kind: "onyx.Toolbar",
-					style: "width: 75%; height: 48px; margin-left: 12.5%; margin-right: 5px; text-align: right;",
-				fit: true,
+			{kind: "Control", style: "-webkit-flex: 1; max-width: 550px;   display: -webkit-flex; -webkit-flex-direction: column;  -webkit-justify-content: center", components:[
+				{name: "bezel", kind: "FittableRows",
+				style: "-webkit-flex: 1; max-height: 665px; border: 5px solid #333; background-color: #777; padding: 10px; color: white; margin: 10px; border-radius: 16px; text-align: right;",
 				components:[
-					{name: "Formula", style: "font-size: 12pt;"}
+					{kind: "onyx.Toolbar", style: "margin-bottom: 5px;", components:[
+					{name: "Result", style: "font-size: 16pt; font-weight: bold;"}
 				]},
-				{kind: "onyx.Button", style: "width: 48px; height: 48px; border-radius: 24px;", classes: "onyx-toolbar", content: "<", ontap: "backspaceTapped"}
-			]},
-			{kind: "FittableRows",
-			fit: true,
-			defaultKind: enyo.kind({
-				kind: "FittableColumns",
-				style: "height: 15.5%; margin: 0.5%;",
-				defaultKind: enyo.kind({
-					kind: "onyx.Button",
-					classes: "onyx-toolbar",
-					style: "width: 24%; margin: 0.5%; border-radius: 8px; font-size: 24pt; font-weight: bold;",
-					ontap: "keyTapped"
-				})
-			}),
-			components:[
-				{components:[
-					/**{style: "visibility: hidden;"},*/
-    					{content: "\u221a", ontap: "sqrtTapped"},		
-					{content: "("},
-					{content: ")"},
-					{content: "C", style: "margin-right: 0;", ontap: "cancelTapped"}
-				]},
- {components:[
-                                        {content: "ln", ontap: "lnTapped"},
-                                        {content: "log", ontap: "logTapped"},
-                                        {content: "^2"},
-                                        {content: "%"}
-                                ]},
-
-				{components:[
-					{content: "7", classes: "number-button"},
-					{content: "8", classes: "number-button"},
-					{content: "9", classes: "number-button"},
-					{content: "+", style: "margin-right: 0;"}
-				]},
-				{components:[
-					{content: "4", classes: "number-button"},
-					{content: "5", classes: "number-button"},
-					{content: "6", classes: "number-button"},
-					{content: "-", style: "margin-right: 0;"}
-				]},
-				{components:[
-					{content: "1", classes: "number-button"},
-					{content: "2", classes: "number-button"},
-					{content: "3", classes: "number-button"},
-					{content: "*", style: "margin-right: 0;"}
-				]},
-				{components:[
-					{content: "."},
-					{content: "0", classes: "number-button"},
-					{content: "=", ontap: "equalsTapped"},
-					{content: "/", style: "margin-right: 0;"}
-				]}
+					{kind: "FittableColumns", style: "margin-bottom: 5px;", components:[
+						{kind: "onyx.Toolbar",
+						style: "width: 75%; height: 48px; margin-left: 12.5%; margin-right: 5px; text-align: right;",
+						fit: true,
+						components:[
+							{name: "Formula", style: "font-size: 12pt;"}
+						]},
+						{kind: "onyx.Button", style: "width: 48px; height: 48px; border-radius: 24px;", classes: "onyx-toolbar", content: "<", ontap: "backspaceTapped"}
+					]},
+					{kind: "FittableRows",
+					fit: true,
+					defaultKind: enyo.kind({
+						kind: "FittableColumns",
+						style: "height: 15.5%; margin: 0.5%;",
+						defaultKind: enyo.kind({
+						kind: "onyx.Button",
+						classes: "onyx-toolbar",
+						style: "width: 24%; margin: 0.5%; border-radius: 8px; font-size: 24pt; font-weight: bold;",
+						ontap: "keyTapped"
+						})
+					}),
+					components:[
+						{components:[
+							/**{style: "visibility: hidden;"},*/
+							{content: "\u221a", ontap: "sqrtTapped"},		
+							{content: "("},
+							{content: ")"},
+							{content: "C", style: "margin-right: 0;", ontap: "cancelTapped"}
+						]},
+						{components:[
+							{content: "ln", ontap: "lnTapped"},
+							{content: "log", ontap: "logTapped"},
+							{content: "^2"},
+							{content: "%", ontap: "percentTapped"}
+						]},
+						{components:[
+							{content: "7", classes: "number-button"},
+							{content: "8", classes: "number-button"},
+							{content: "9", classes: "number-button"},
+							{content: "+", style: "margin-right: 0;"}
+						]},
+						{components:[
+							{content: "4", classes: "number-button"},
+							{content: "5", classes: "number-button"},
+							{content: "6", classes: "number-button"},
+							{content: "-", style: "margin-right: 0;"}
+						]},
+						{components:[
+							{content: "1", classes: "number-button"},
+							{content: "2", classes: "number-button"},
+							{content: "3", classes: "number-button"},
+							{content: "*", style: "margin-right: 0;"}
+						]},
+						{components:[
+							{content: "."},
+							{content: "0", classes: "number-button"},
+							{content: "=", ontap: "equalsTapped"},
+							{content: "/", style: "margin-right: 0;"}
+						]}
+					
+					]}
+				]}   // end bezel
 			]}
-		]}   // end bezel
-		]}
 		]},   // end tabletop
 		{kind: "CoreNavi", fingerTracking: true}
 	],
@@ -132,7 +132,12 @@ enyo.kind({
 			string = ".0" + string;
 			this.$.Formula.setContent(string);
 		}else{
-			var res = string.slice(0,string.length - 2); 
+			var res = string.slice(0,string.length -2 );
+			if(isNaN(string.substr(-2,2)) === true){
+				var lastNum = string.substr(-1,1);
+				this.$.Formula.setContent(string.slice(0,string.length -1 ) + ".0" + lastNum);
+				return;
+			}				// if last two digits are not NaN
 			this.$.Formula.setContent(res + "." + string.substr(-2,2));
 		}
 		
