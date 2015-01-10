532b78fc54001ee344652ac405ae9d7b0be2e14b
john.microtech@gmail.com <john.microtech@gmail.com>
2015-01-10 10:44:28 -0500

    Up date the tools dir and add framework_config.json file
    Open-WebOS-DCO-1.0-Signed-Off-By: john mcconnell  johnmcconnell@yahoo.com

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+# Alert the user of a failed build
+errored () {
+	errcode=$?
+	echo "Deploy encountered errors."
+	exit $errcode
+}
+
+trap errored ERR
+
 # the folder this script is in (*/bootplate/tools)
 TOOLS=$(cd `dirname $0` && pwd)
 
+# application root
+SRC="$TOOLS/.."
+
 # enyo location
-ENYO="$TOOLS/../enyo"
+ENYO="$SRC/enyo"
 
 # deploy script location
 DEPLOY="$ENYO/tools/deploy.js"
@@ -12,9 +24,27 @@ DEPLOY="$ENYO/tools/deploy.js"
 # check for node, but quietly
 if command -v node >/dev/null 2>&1; then
 	# use node to invoke deploy with imported parameters
-	echo "enyo/tools/minify.sh args: " $@
-	node $DEPLOY $@
+	echo "node $DEPLOY -T -s $SRC -o $SRC/deploy $@"
+	node "$DEPLOY" -T -s "$SRC" -o "$SRC/deploy" $@
 else
 	echo "No node found in path"
 	exit 1
 fi
+
+# copy files and package if deploying to cordova webos
+while [ "$1" != "" ]; do
+	case $1 in
+		-w | --cordova-webos )
+			# copy appinfo.json and cordova*.js files
+			DEST="$TOOLS/../deploy/"${PWD##*/}
+			
+			cp "$SRC"/appinfo.json "$DEST" -v
+			cp "$SRC"/cordova*.js "$DEST" -v
+			
+			# package it up
+			mkdir -p "$DEST/bin"
+			palm-package "$DEST/bin"
+			;;
+	esac
+	shift
+done
