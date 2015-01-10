4aa9804f247ac56ac457706b800706cd5ff11b3c
john.microtech@gmail.com <john.microtech@gmail.com>
2015-01-10 10:51:23 -0500

    Fix the menu so it working again
    Open-WebOS-DCO-1.0-Signed-Off-By: john mcconnell  johnmcconnell@yahoo.com

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 node enyo/tools/deploy.js -o deploy/org.webosports.app.calculator
 adb push deploy/org.webosports.app.calculator /usr/palm/applications/org.webosports.app.calculator
-read -p "Paused  Press [Enter] key to restart and End..."
+
+adb shell luna-send -n 1 luna://com.palm.applicationManager/rescan {}
+
+adb shell systemctl restart luna-next
